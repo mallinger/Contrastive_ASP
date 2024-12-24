dc904df1252bde2611d2e7b63451aaaa923f764f
mallinger <e1046033@student.tuwien.ac.at>
2024-12-24 22:28:26 +0100

    Minor changes

--- grounder.py
+++ grounder.py
@@ -25,6 +25,9 @@ def expand_range(program_string):
 def ground(program_string):
     program_string = program_string.strip()
 
+    if re.search(r"(?<!\d)\.\.", program_string) is not None or re.search(r"\.\.(?!\d)", program_string) is not None:
+        raise SyntaxError("Program contains invalid range notation.")
+        
     while ".." in program_string:
         program_string = expand_range(program_string).strip()
 
