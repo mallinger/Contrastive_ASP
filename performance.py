f70b5f90549d79dcfbaf54bc37dcbbafc4aae8d3
mallinger <e1046033@student.tuwien.ac.at>
2025-11-07 17:18:20 +0100

    updated coloring performance testing

--- performance.py
+++ performance.py
@@ -14,14 +14,21 @@ def test(name, example, n):
         with open("performance_results/result.txt", "a") as result_file:
             result_file.write(f"{name}, {n}, {time_stop - time_start}\n")
 
-if __name__ == '__main__':
-    for n in range(60, 100,5):
-        test("coloring", ex_coloring, n)
-    for n in range(4, 16):
-        test("nqueens", ex_nqueens, n)
-    for n in (4, 9, 16, 25):
-        test("sudoku", ex_sudoku, n)
-    
+def test_coloring():
+    for n in range(120, 126, 5):
+        for i in range(1,4):
+            explanation_frame, contrastive_explanation_problem = ex_coloring(n, i)
+            time_start = perf_counter()
+            contrastive_explanations(explanation_frame, contrastive_explanation_problem)
+            time_stop = perf_counter()   
+            with open("performance_results/result.txt", "a") as result_file:
+                result_file.write(f"coloring, {n}-{i}, {round(time_stop - time_start, 4)}\n")
 
+if __name__ == '__main__':
+    test_coloring()
+    for n in range(4, 35):
+        test("nqueens", ex_nqueens, n)
+    for n in  (4, 9, 16, 25):
+        test("sudoku", ex_sudoku_simplified, n)
    
         
